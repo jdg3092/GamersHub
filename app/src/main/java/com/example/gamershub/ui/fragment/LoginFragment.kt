0ffa653cfdbf95931f79d0699fdd75dfde6871f0
jdg3092 <suso24jd@gmail.com>
2025-05-01 12:17:50 +0200

    (feat) LoginFragment logic v.1.0 and  Logo v.1.0 add

--- app/src/main/java/com/example/gamershub/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/gamershub/ui/fragment/LoginFragment.kt
@@ -1,14 +1,26 @@
 package com.example.gamershub.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.gamershub.R
 import com.example.gamershub.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        auth = FirebaseAuth.getInstance()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,5 +32,23 @@ class LoginFragment: Fragment() {
 
     override fun onStart() {
         super.onStart()
+        binding.btnLogin.setOnClickListener {
+            auth.signInWithEmailAndPassword(
+                binding.editCorreo.text.toString(),
+                binding.editPass.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+
+                } else {
+                    Snackbar.make(binding.root, "Error al iniciar sesión", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+        binding.textRegistro.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
     }
+
+
 }
