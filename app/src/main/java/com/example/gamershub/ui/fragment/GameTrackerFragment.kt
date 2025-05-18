7d7a3fe7f47583788959c9187855beef44deedbb
jdg3092 <suso24jd@gmail.com>
2025-05-18 12:59:41 +0200

    Update GameTracker

--- app/src/main/java/com/example/gamershub/ui/fragment/GameTrackerFragment.kt
+++ app/src/main/java/com/example/gamershub/ui/fragment/GameTrackerFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.gamershub.R
 import com.example.gamershub.databinding.FragmentGametrackerBinding
 
-class GameTrackerFragment: Fragment() {
+class GameTrackerFragment : Fragment() {
     private lateinit var binding: FragmentGametrackerBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,5 +19,27 @@ class GameTrackerFragment: Fragment() {
     ): View? {
         binding = FragmentGametrackerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.toolbarGameTracker.inflateMenu(R.menu.menu_gametracker)
+        binding.toolbarGameTracker.overflowIcon?.setTint(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.white
+            )
+        )
+        binding.toolbarGameTracker.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.volverMainFragmnet -> {
+                    findNavController().navigate(R.id.action_gameTrackerFragment_to_mainFragment)
+                    return@setOnMenuItemClickListener true
+                }
+
+            }
+            return@setOnMenuItemClickListener true
+
+        }
     }
 }
