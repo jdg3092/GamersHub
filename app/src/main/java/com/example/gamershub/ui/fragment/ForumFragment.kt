062d6958916a6dbea4c25dd7a7468b272560e456
jdg3092 <suso24jd@gmail.com>
2025-05-21 17:56:48 +0200

    Update ForumFragment

--- app/src/main/java/com/example/gamershub/ui/fragment/ForumFragment.kt
+++ app/src/main/java/com/example/gamershub/ui/fragment/ForumFragment.kt
@@ -1,6 +1,45 @@
 package com.example.gamershub.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.gamershub.R
+import com.example.gamershub.databinding.FragmentForumBinding
 
-class ForumFragment: Fragment()  {
+class ForumFragment : Fragment() {
+    private lateinit var binding: FragmentForumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentForumBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.toolbarForum.inflateMenu(R.menu.menu_forum)
+        binding.toolbarForum.overflowIcon?.setTint(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.white
+            )
+        )
+        binding.toolbarForum.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.volverMainFragmnet -> {
+                    findNavController().navigate(R.id.action_forumFragment_to_mainFragment)
+                    return@setOnMenuItemClickListener true
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+    }
+
 }
